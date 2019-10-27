5649672eafdd2f702410657e9afc78e8eba79776
Carrie MacDonald <carrie8783@gmail.com>
2019-10-27 17:02:55 -0400

    Adding initial project files after completing React + Parcel set-up

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ParcelLogo from "../img/parcel-logo.png";
+
+const Header = () => (
+    <header>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+                  <img src={ParcelLogo} alt="Screenshot"/>
+                </a>
+            </div>
+        </nav>
+    </header>
+)
+
+export default Header
