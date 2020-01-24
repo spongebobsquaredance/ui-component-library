150d2b5b104f0ed3c695930372de01ffe3ea8d16
Carrie MacDonald <carrie8783@gmail.com>
2020-01-23 20:10:59 -0500

    Avatar: added 6 style variations to Avatar story

--- src/components/Avatar/Avatar.stories.js
+++ src/components/Avatar/Avatar.stories.js
@@ -2,13 +2,65 @@ import React from 'react';
 import Avatar from './Avatar';
 import BgImage from './../../img/sample-icon.png';
 
+const small = '40px';
+const medium = '64px';
 const large = '80px';
-const radius = '18px';
+
+const circle = '100%';
+
+const squaredSmall = '12px';
+const squaredMedium = '18px';
+const squaredLarge = '24px';
 
 export default { title: 'Avatar' };
 
-export const avatarComponent = () => <Avatar
-  image={BgImage}
-  radius={radius}
-  size={large}
- />;
+const AvatarCircleSmall = () =>
+  <Avatar
+    image={BgImage}
+    radius={circle}
+    size={small}
+  />
+
+const AvatarCircleMedium = () =>
+  <Avatar
+    image={BgImage}
+    radius={circle}
+    size={medium}
+  />
+
+const AvatarCircleLarge = () =>
+  <Avatar
+    image={BgImage}
+    radius={circle}
+    size={large}
+  />
+
+const AvatarSquaredSmall = () =>
+  <Avatar
+    image={BgImage}
+    radius={squaredSmall}
+    size={small}
+  />
+
+const AvatarSquaredMedium = () =>
+  <Avatar
+    image={BgImage}
+    radius={squaredMedium}
+    size={medium}
+  />
+
+const AvatarSquaredLarge = () =>
+  <Avatar
+    image={BgImage}
+    radius={squaredLarge}
+    size={large}
+  />
+
+export { 
+  AvatarCircleSmall,
+  AvatarCircleMedium,
+  AvatarCircleLarge,
+  AvatarSquaredSmall,
+  AvatarSquaredMedium,
+  AvatarSquaredLarge
+}
