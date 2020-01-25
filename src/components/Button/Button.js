6d10b80205bba7c80a129162cf866fe0bf4f3e2f
Carrie MacDonald <carrie8783@gmail.com>
2020-01-25 17:28:34 -0500

    Button: formatting JS file, removing unneeded comments

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// variation: ghost
-// background: ${p => p.theme.color.backgroundLight};
-// color: ${p => p.theme.color.accent};
-// hover and focus: default colors
-// {this.state.hiring ? <Hiring /> : <NotHiring />}
-// ${p => p.ghost ? p.theme.color.backgroundLight : p.theme.color.accent};
-// Default styles
-
 const Link = styled.a`
-  background: ${p => p.ghost ? p.theme.color.backgroundLight : p.theme.color.accent};
+  background: ${p => p.ghost ?
+                p.theme.color.backgroundLight :
+                p.theme.color.accent};
   border: 2px solid ${p => p.theme.color.accent};
   border-radius: 6px;
-  color: ${p => p.ghost ? p.theme.color.accent : p.theme.color.backgroundLight};
+  color: ${p => p.ghost ?
+           p.theme.color.accent :
+           p.theme.color.backgroundLight};
   display: inline-block;
-  font-size: ${p => p.small ? p.theme.fontSize.buttonSmall : p.theme.fontSize.button};
+  font-size: ${p => p.small ?
+               p.theme.fontSize.buttonSmall : 
+               p.theme.fontSize.button};
   font-weight: 700;
-  line-height: ${p => p.small ? p.theme.lineHeight.buttonSmall : p.theme.lineHeight.button};
+  line-height: ${p => p.small ?
+                 p.theme.lineHeight.buttonSmall :
+                 p.theme.lineHeight.button};
   min-width: ${p => p.small ? '180px' : '340px'};
   padding: ${p => p.small ? '11px' : '13px 16px'};
   text-align: center;
@@ -27,12 +27,22 @@ const Link = styled.a`
   -webkit-font-smoothing: antialiased;
   &:hover,
   &:focus {
-    background: ${p => p.ghost ? p.theme.color.accent : p.theme.color.backgroundLight};
-    color: ${p => p.ghost ? p.theme.color.backgroundLight : p.theme.color.accent};
+    background: ${p => p.ghost ?
+                  p.theme.color.accent :
+                  p.theme.color.backgroundLight};
+    color: ${p => p.ghost ?
+             p.theme.color.backgroundLight :
+             p.theme.color.accent};
   }
 `
 
-const Button = ({href, title, small, ghost, buttonText}) =>
+const Button = ({
+  href, 
+  title, 
+  small, 
+  ghost, 
+  buttonText
+}) =>
   <Link 
     href={href} 
     title={title} 
